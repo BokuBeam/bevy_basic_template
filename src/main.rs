7a3437aae7dbc1302cb1405cf318cade48465ab2
bokubeam <bokubeam@gmail.com>
2022-03-29 12:26:31 -0700

    Add diagnostic plugins

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use bevy::prelude::*;
+use bevy_editor_pls::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EditorPlugin)
+        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+        .run();
 }
